7495026e8843e4d21ba7ecf02c986e05e47acc22
Sasha Bullet <sashabullet@gmail.com>
2019-12-06 15:20:13 +0200

    removed comments

--- github_registration_test.js
+++ github_registration_test.js
@@ -32,39 +32,5 @@ describe('angularjs homepage', function() {
     var EC = protractor.ExpectedConditions;
     browser.wait(EC.textToBePresentInElement($('h2'), 'Verify your account'), 5000);
 
-
-
-
-
-
-
   });
-
-
-
-  // describe('todo list', function() {
-  //   var todoList;
-  //
-  //   beforeEach(function() {
-  //     browser.get('http://www.angularjs.org');
-  //
-  //     todoList = element.all(by.repeater('todo in todoList.todos'));
-  //   });
-  //
-  //   it('should list todos', function() {
-  //     expect(todoList.count()).toEqual(2);
-  //     expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
-  //   });
-  //
-  //   it('should add a todo', function() {
-  //     var addTodo = element(by.model('todoList.todoText'));
-  //     var addButton = element(by.css('[value="add"]'));
-  //
-  //     addTodo.sendKeys('write a protractor test');
-  //     addButton.click();
-  //
-  //     expect(todoList.count()).toEqual(3);
-  //     expect(todoList.get(2).getText()).toEqual('write a protractor test');
-  //   });
-  // });
 });
