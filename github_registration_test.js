abf970d0ec1b3d43ae6bb56807d533f0e8b37f19
Sasha Bullet <sashabullet@gmail.com>
2019-12-12 11:05:49 +0200

    wrote test with using page object pattern

--- github_registration_test.js
+++ github_registration_test.js
@@ -1,36 +1,37 @@
+var pageObject = {
+  loginButton: function () {
+    var form = element(by.css('.home-hero-signup'));
+    return form.element(by.css('button[type="submit"]'));
+  },
+  usernameInput: function () {
+    return element(by.id('user[login]'));
+  },
+  emailInput: function () {
+    return element(by.id('user[email]'));
+  },
+  passwordInput: function () {
+    return element(by.id('user[password]'));
+  }
+}
+
 describe('angularjs homepage', function() {
-  it('should greet the named user', function() {
+	
+	browser.waitForAngularEnabled(false);
+  browser.get('http://github.com');
 
-
-    browser.waitForAngularEnabled(false);
-    browser.get('http://github.com');
-
-    var username_input = element(by.id('user[login]'));
-    var until = protractor.ExpectedConditions;
-
-    browser.wait(until.presenceOf(username_input));
-
-
-    username_input.sendKeys('Alex-bullet');
-    var Email_input = element(by.id("user[email]"));
-    browser.wait(until.presenceOf(Email_input));
-
-
-    Email_input.sendKeys('hgafsghxfa@weqh24.com');
-
-    var Password_input = element(by.id("user[password]"));
-    browser.wait(until.presenceOf(Password_input));
-
-    Password_input.sendKeys('2LDQj9ewuWPP');
-
-    browser.wait(element(by.buttonText('Sign up for GitHub')));
-
-
-    element(by.buttonText('Sign up for GitHub')).click();
-
-    // Waits for the element with id 'h2' to contain the text 'Verify your account'.
-    var EC = protractor.ExpectedConditions;
-    browser.wait(EC.textToBePresentInElement($('h2'), 'Verify your account'), 5000);
-
-  });
+	var EC = protractor.ExpectedConditions;
+	browser.wait(EC.presenceOf(pageObject.loginButton()));
+	
+	it('should greet the named user', function(done) {
+		pageObject.usernameInput().sendKeys('Alex-bullet');
+		pageObject.emailInput().sendKeys('hgafsghxfa@weqh24.com');
+		pageObject.passwordInput().sendKeys('2LDQj9ewuWPP');
+		pageObject.loginButton().click();
+    
+		setTimeout(function () {
+      expect(browser.getCurrentUrl()).toBe('https://github.com/join');
+      browser.close();
+			done();
+		}, 5000);
+	});
 });
